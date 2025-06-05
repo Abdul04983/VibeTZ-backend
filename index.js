43a294fbe3e8f7ee6a3acb98acad145775989dfa
Abdul Bashiru <abdulbashiru598@gmail.com>
2025-06-05 14:09:03 +0300

    Fix: define socket properly and update index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-import messageRoutes from "./routes/messageRoutes.js";
-   socket.on("sendMessage", (data) => {
-  const { receiverId } = data;
-  socket.to(receiverId).emit("receiveMessage", data);
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
-app.use("/api/messages", messageRoutes);
+
+// Socket.io logic inside connection
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("sendMessage", (data) => {
+    console.log("Message received:", data);
+    io.emit("receiveMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
